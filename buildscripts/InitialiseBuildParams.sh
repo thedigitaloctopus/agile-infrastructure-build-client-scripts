42973154bd753c540acd5aab4ec6a63dc937bcf1
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-02-21 21:53:20 +0000

    Update InitialiseBuildParams.sh

--- buildscripts/InitialiseBuildParams.sh
+++ buildscripts/InitialiseBuildParams.sh
@@ -157,7 +157,8 @@ status "IF YOU USE MULTIPLE WEBSERVERS, AND SESSIONS ARE NOT PERSISTED TO THE DA
 status "THEM BY SELECTING THIS OPTION AND THE SESSION PATH IN YOUR APPLICATION SET TO THE SHARED IMAGE/MEDIA DIRECTORY PATH SO THE"
 status "SESSION DATA CAN BE WRITTEN THERE"
 status "####################################################################################################################################################"
-status "Do you wish to store your assets in a shared filesystem either in the cloud of with an elastic file system when supported?"
+status "Do you wish to store your assets in a shared filesystem either in the cloud or with an elastic file system when supported?"
+status "Note: if your application has the capability to use CDNs, then, you might not need to use this option as your application will deal with it"
 status "Please enter (Y|y) or (N|n)"
 read response
 
