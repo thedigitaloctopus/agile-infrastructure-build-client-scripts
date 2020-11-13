7e87dd8b68baed947e8d17ebc2102c2e678de53a
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-13 11:53:52 +0000

    Update GetServerTypeID.sh

--- providerscripts/server/GetServerTypeID.sh
+++ providerscripts/server/GetServerTypeID.sh
@@ -81,7 +81,7 @@ if ( [ "${cloudhost}" = "vultr" ] )
 then
     export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${cloudhost}/TOKEN`"
     /bin/sleep 1
-    /usr/bin/vultr plans | grep "${server_size}" | /bin/grep SSD | /usr/bin/awk '{print $1}' | /usr/bin/sort | /usr/bin/head -1
+    /usr/bin/vultr plans | grep "${server_size}" | /bin/grep SSD | /usr/bin/awk '{print $1}' | /usr/bin/sort | /usr/bin/tail -1
 fi
 
 if ( [ "${cloudhost}" = "aws" ] )
