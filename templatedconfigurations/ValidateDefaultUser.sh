aa73e1d3f5154d8808f17bec19bfc91ce1b5bd79
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-21 11:50:56 +0000

    Update ValidateDefaultUser.sh

--- templatedconfigurations/ValidateDefaultUser.sh
+++ templatedconfigurations/ValidateDefaultUser.sh
@@ -1,3 +1,24 @@
+#!/bin/sh
+###################################################################################
+# Description : This will validate the default user as set in the template
+# Author: Peter Winter
+# Date  : 13/07/2020
+###################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+
 while ( [ "${CLOUDHOST}" = "digitalocean" ] && [ "${DEFAULT_USER}" != "root" ] )
 do
     status "######################################################################################################################"
