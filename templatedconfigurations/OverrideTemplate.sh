6fc528e635c05dbe42b51d1ac808e036f0e3d3df
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-25 16:04:05 +0000

    Create OverrideTemplate.sh

---
+++ templatedconfigurations/OverrideTemplate.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#set -x
+
+if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+then
+        :
+fi
+if ( [ "${CLOUDHOST}" = "exoscale" ] )
+then
+        :
+fi
+if ( [ "${CLOUDHOST}" = "linode" ] )
+then
+    while read line
+    do
+        variables="${variables} `/bin/echo ${line} | /usr/bin/awk -F'"' '{print $2}' | /bin/sed '/^$/d'`"
+    done < /root/StackScript
+
+    for variable in ${variables}
+    do
+        /bin/sed -i "/${variable}=/d" ${templatefile}
+        eval "payload=\${$variable}"
+        /bin/echo "export ${variable}=\"${payload}\"" >> ${templatefile}
+    done
+fi
+if ( [ "${CLOUDHOST}" = "vultr" ] )
+then
+        :
+fi
+if ( [ "${CLOUDHOST}" = "aws" ] )
+then
+        :
+fi
