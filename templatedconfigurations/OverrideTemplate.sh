b8cf3dc7769a5b9befcd49eeeb69ce37e0b21a47
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-26 10:52:12 +0000

    Update OverrideTemplate.sh

--- templatedconfigurations/OverrideTemplate.sh
+++ templatedconfigurations/OverrideTemplate.sh
@@ -4,7 +4,17 @@
 
 if ( [ "${CLOUDHOST}" = "digitalocean" ] )
 then
-        :
+    while read line
+    do
+        variables="${variables} `/bin/echo ${line} | /bin/grep "^export" | /usr/bin/awk -F'"' '{print $2}' | /bin/sed '/^$/d'`"
+    done < /root/StackScript
+
+    for variable in ${variables}
+    do
+        /bin/sed -i "/${variable}=/d" ${templatefile}
+        eval "payload=\${$variable}"
+        /bin/echo "export ${variable}=\"${payload}\"" >> ${templatefile}
+    done
 fi
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
