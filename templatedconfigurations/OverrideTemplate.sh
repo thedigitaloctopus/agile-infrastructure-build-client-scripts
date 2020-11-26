f56590695d85defa9300627f7bf14d6328b9cc49
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-26 19:27:39 +0000

    Update OverrideTemplate.sh

--- templatedconfigurations/OverrideTemplate.sh
+++ templatedconfigurations/OverrideTemplate.sh
@@ -46,10 +46,22 @@ then
         /bin/echo "export ${variable}=\"${payload}\"" >> ${templatefile}
     done
 fi
+
 if ( [ "${CLOUDHOST}" = "vultr" ] )
 then
-        :
+    while read line
+    do
+        variables="${variables} `/bin/echo ${line} | /bin/grep "^export" | /usr/bin/awk -F'=' '{print $1}' | /usr/bin/awk '{print $NF}' | /bin/sed '/^$/d'`"
+    done < /root/Environment.env
+
+    for variable in ${variables}
+    do
+        /bin/sed -i "/${variable}=/d" ${templatefile}
+        eval "payload=\${$variable}"
+        /bin/echo "export ${variable}=\"${payload}\"" >> ${templatefile}
+    done
 fi
+
 if ( [ "${CLOUDHOST}" = "aws" ] )
 then
         :
