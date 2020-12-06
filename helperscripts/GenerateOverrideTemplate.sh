cf7f9a270e38a04977ac2632fe4f0470017f6279
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-12-06 17:49:21 +0000

    Update GenerateOverrideTemplate.sh

--- helperscripts/GenerateOverrideTemplate.sh
+++ helperscripts/GenerateOverrideTemplate.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
-
+#######################################################################################################
+# Author : Peter Winter
+# Date   : 06/12/2020
+# Description : With this script you can generate your template overrides init script interactively 
+# rather than having to edit the template overrides file directly. Once this script has run, you can
+# copy the scipt output into the userdata part of your build compute instance for your provider and it
+# will spin up all the infrastructure for your build. 
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
 #set -x
 
 if ( [ ! -f  ./GenerateOverrideTemplate.sh ] )
