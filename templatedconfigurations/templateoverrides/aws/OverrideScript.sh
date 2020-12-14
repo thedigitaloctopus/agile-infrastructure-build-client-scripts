cdaab9ca205acf53057caeae6844647a3628e1d8
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-12-14 20:15:55 +0000

    Update OverrideScript.sh

--- templatedconfigurations/templateoverrides/aws/OverrideScript.sh
+++ templatedconfigurations/templateoverrides/aws/OverrideScript.sh
@@ -7,7 +7,7 @@
 ####################################################################################################################################################################
 /bin/echo "
 #BASE OVERRIDES
-export SSH="" #paste your public key here
+export SSH=\"\" #paste your public key here
 export BUILDOS="debian" #one of ubuntu|debian
 export BUILDOS_VERSION="10" #one of 20.04|10
 export DEFAULT_USER="admin" # - this should be "ubuntu" if you are deploying BUILDOS ubuntu and "admin" if you are deploying BUILDOS debian
