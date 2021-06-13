69ed85a150b72fdaec47bda981812c3cd607ea57
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-13 14:17:10 +0100

    Update SelectCloudhostExpedited.sh

--- SelectCloudhostExpedited.sh
+++ SelectCloudhostExpedited.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+######################################################################################################################
+# Description: This is the script which asks which of the supported cloud host providers the user wishes to deploy to
+# for the expedited deployment process
+# Author: Peter Winter
+# Date: 13/06/2021
+######################################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+######################################################################################################
+#set -x
 
 status "####################################################################################"
 status "Please tell me which of the supported cloudhosts you are deploying to"
