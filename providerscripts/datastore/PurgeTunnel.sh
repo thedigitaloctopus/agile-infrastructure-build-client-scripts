5ba3542ad1a4cdc78762e346c192ea555d68ecb2
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-14 02:19:04 +0100

    Update PurgeTunnel.sh

--- providerscripts/datastore/PurgeTunnel.sh
+++ providerscripts/datastore/PurgeTunnel.sh
@@ -26,6 +26,7 @@ then
     config_bucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`-config/*"
     if ( [ "`/usr/bin/s3cmd ls s3://${config_bucket}`" != "" ] )
     then
+        status "Purging bucket ${config_bucket}... feel free to purge it manually through the GUI if you want to"
         /usr/bin/s3cmd --recursive --force del s3://${config_bucket}/*
     fi
 
