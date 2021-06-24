d74bd248b4ccb6849ab299bbc7f5c9d0210336c7
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-24 20:09:34 +0100

    Update ConnectToWebserver.sh

--- helperscripts/ConnectToWebserver.sh
+++ helperscripts/ConnectToWebserver.sh
@@ -35,18 +35,23 @@ read response
 if ( [ "${response}" = "1" ] )
 then
     CLOUDHOST="digitalocean"
+    token_to_match="webserver*"
 elif ( [ "${response}" = "2" ] )
 then
     CLOUDHOST="exoscale"
+    token_to_match="webserver*"
 elif ( [ "${response}" = "3" ] )
 then
     CLOUDHOST="linode"
+    token_to_match="webserver*"
 elif ( [ "${response}" = "4" ] )
 then
     CLOUDHOST="vultr"
+    token_to_match="webserver*"
 elif ( [ "${response}" = "5" ] )
 then
     CLOUDHOST="aws"
+    token_to_match="webserver*"
 else
     /bin/echo "Unrecognised  cloudhost. Exiting ...."
     exit
@@ -58,7 +63,7 @@ fi
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
 
-ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh webserver* ${CLOUDHOST} ${BUILD_HOME}`"
+ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
 
 if ( [ "${ips}" = "" ] )
 then
