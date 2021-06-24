a5640ae1c7b9ccc0760a5d28de326feea55680fa
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-24 18:36:06 +0100

    Update GetServerIPAddresses.sh

--- providerscripts/server/GetServerIPAddresses.sh
+++ providerscripts/server/GetServerIPAddresses.sh
@@ -30,8 +30,10 @@ fi
 
 if ( [ "${cloudhost}" = "exoscale" ] )
 then
-        /usr/local/bin/cs listVirtualMachines | /usr/bin/jq '.virtualmachine[] | .nic[].ipaddress + " " + .displayname' | /bin/grep ".*${server_type}" | /bin/sed 's/"//g' | /usr/bin/awk '{print $1}'
-fi
+     #ORIGINAL   /usr/local/bin/cs listVirtualMachines | /usr/bin/jq '.virtualmachine[] | .nic[].ipaddress + " " + .displayname' | /bin/grep ".*${server_type}" | /bin/sed 's/"//g' | /usr/bin/awk '{print $1}'
+     display_name="${server_type}"
+     ip="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_display_name "${display_name}" '(.virtualmachine[] | select(.displayname == $tmp_display_name) | .publicip)' | /bin/sed 's/"//g'`"
+     /bin/echo ${ip}fi
 
 if ( [ "${cloudhost}" = "linode" ] )
 then
