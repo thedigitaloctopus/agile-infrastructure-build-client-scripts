254a36b1d7283a61579d78e09d4dbe844fe0ad7b
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-28 18:43:32 +0100

    Update BuildDatabase.sh

--- buildscripts/BuildDatabase.sh
+++ buildscripts/BuildDatabase.sh
@@ -226,10 +226,10 @@ do
         #This key is the key that was generated when we wish to use DBaaS over an SSH tunnel. This key gets us access to the remote end
         #of our ssh tunnel and from there we are port forwarded to our database which is running as a service.
 
-        if ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS-secured" ] )
-        then
-            /usr/bin/scp ${OPTIONS} ${BUILD_HOME}/ssl/${WEBSITE_URL}/dbaas_server_key.pem ${DEFAULT_USER}@${ip}:/home/${SERVER_USER}/.ssh/dbaas_server_key.pem
-        fi
+      #  if ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS-secured" ] )
+      #  then
+      #      /usr/bin/scp ${OPTIONS} ${BUILD_HOME}/ssl/${WEBSITE_URL}/dbaas_server_key.pem ${DEFAULT_USER}@${ip}:/home/${SERVER_USER}/.ssh/dbaas_server_key.pem
+      #  fi
 
         #Harden ourselves by switching off root based authentication. After this, we cannot remotely access this machine as root, even if we want to
         /usr/bin/ssh ${OPTIONS} ${DEFAULT_USER}@${ip} "DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get update ; /usr/bin/apt-get install sudo ; /bin/sh -c '${SUDO} /bin/chown -R ${SERVER_USER}.${SERVER_USER} /home/${SERVER_USER}/ ; ${SUDO} /bin/chmod 700 /home/${SERVER_USER}/.ssh ; ${SUDO} /bin/chmod 400 /home/${SERVER_USER}/.ssh/authorized_keys' ; ${SUDO} /bin/sed -i '$ a\ ClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 10000' /etc/ssh/sshd_config ; ${SUDO} /bin/sed -i 's/.*PermitRootLogin.*$/PermitRootLogin no/g' /etc/ssh/sshd_config ;  ${SUDO} /usr/sbin/service sshd restart"
