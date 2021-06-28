f5ed5a36b16b8eead0c27505d8c2fd858aa81066
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-28 18:53:56 +0100

    Update PreProcessingMessages.sh

--- PreProcessingMessages.sh
+++ PreProcessingMessages.sh
@@ -113,7 +113,7 @@ then
     fi
 fi
 
-if ( [ "${APPLICATION}" = "moodle" ] && ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] || [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS-secured" ] && [ "${DATABASE_DBaaS_INSTALLATION_TYPE}" = "Maria" ] ) )
+if ( [ "${APPLICATION}" = "moodle" ] && ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] || [ "${DATABASE_DBaaS_INSTALLATION_TYPE}" = "Maria" ] ) )
 then
     status "###################################################################################################################################"
     status "Hi, it's me again... I am going to try and set some parameters for your database. Some providers, for example, AWS, don't"
