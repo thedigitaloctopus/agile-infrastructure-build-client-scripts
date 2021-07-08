3d204e214521d05bab1232da7cbf95af4c1a1627
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-08 19:56:39 +0100

    Update VirginJoomlaOverride.sh

--- templatedconfigurations/templateoverrides/linode/VirginJoomlaOverride.sh
+++ templatedconfigurations/templateoverrides/linode/VirginJoomlaOverride.sh
@@ -32,18 +32,65 @@
 # <UDF name="SELECTED_TEMPLATE" label="The ADT template number to build from" oneof="2"/ default="2">
 # <UDF name="PHP_VERSION" label="Which PHP Version do you want to deploy to?" oneof="7.0,7.1,7.2,7.3,7.4,8.0" default="7.4"/>
 #######################################################################################################
-#This is the main script. Ensure the SSH key is authorised in case it hasn't been set yet. Also disable password based authentication
-/bin/mkdir ~/.ssh
-/bin/echo "${SSH}" >> ~/.ssh/authorized_keys
+###############################################################################################
+# SET THESE FOR YOUR BUILD CLIENT MACHINE
+# THESE WILL BE THE USERNAME THE PASSWORD YOU CAN USE TO DO A SUDO ONCE AUTHENTICATED AND THE SSH PORT TO CONNECT ON
+# CONNECT TO YOUR BUILD CLIENT MACHINE AS FOLLOWS:
+# ssh -i <ssh-private-key> -p ${BUILDCLIENT_SSH_PORT} $BUILDCLIENT_USER@<buildclientip>
+# $BUILDCLIENT_USER>sudo su
+# password:${BUILDCLIENT_PASSWORD}
+# cd agile-infrastructure-build-client-scripts/logs
+# tail -f build*out*
+####################################################################################
+export BUILDCLIENT_USER="agile-deployer"
+export BUILDCLIENT_PASSWORD="mnbcxz098321QQZZ"
+export BUILDCLIENT_SSH_PORT="1035"
+
+/usr/sbin/adduser --disabled-password --gecos \"\" ${BUILDCLIENT_USER} 
+/bin/sed -i '$ a\ ClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 10000' /etc/ssh/sshd_config
+/bin/sed -i 's/.*PermitRootLogin.*$/PermitRootLogin no/g' /etc/ssh/sshd_config
+/bin/echo ${BUILDCLIENT_USER}:${BUILDCLIENT_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/chpasswd 
+ /usr/bin/gpasswd -a ${BUILDCLIENT_USER} sudo 
+
+/bin/mkdir -p /home/${BUILDCLIENT_USER}/.ssh
+/bin/echo "${SSH}" >> /home/${BUILDCLIENT_USER}/.ssh/authorized_keys
+
 /bin/sed -i 's/#*PasswordAuthentication [a-zA-Z]*/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+if ( [ "${BUILDCLIENT_SSH_PORT}" = "" ] )
+then
+    BUILDCLIENT_SSH_PORT="22"
+fi
+
+/bin/sed -i "s/^Port.*$/Port ${BUILDCLIENT_SSH_PORT}/g" /etc/ssh/sshd_config
+/bin/sed -i "s/^#Port.*$/Port ${BUILDCLIENT_SSH_PORT}/g" /etc/ssh/sshd_config
+
 systemctl restart sshd
 service ssh restart
-#Install the git software
+
 /usr/bin/apt -qq -y update
 /usr/bin/apt -qq -y install git
-cd /root
-#Clone the Agile Deployment Toolkit
-/usr/bin/git clone https://github.com/agile-deployer/agile-infrastructure-build-client-scripts.git
+/usr/bin/apt -qq -y install ufw
+
+/usr/sbin/ufw enable
+/usr/sbin/ufw default deny incoming
+/usr/sbin/ufw default allow outgoing
+####################################################################################################################################################
+#It is possible to lock down ssh connections to only from a specific ip address which is more secure, but, if the IP address of your machine changes,
+#for example, if you connect your laptop to a different network, then, you will have to connect to the build client machine through the console of
+#your VPS system provider and allow your new IP address through the firewall. This might be more of a hassle than its worth
+#####################################################################################################################################################
+/usr/sbin/ufw allow ${BUILDCLIENT_SSH_PORT}/tcp 
+
+cd /home/${BUILDCLIENT_USER}
+
+if ( [ "${INFRASTRUCTURE_REPOSITORY_OWNER}" != "" ] )
+then
+    /usr/bin/git clone https://github.com/${INFRASTRUCTURE_REPOSITORY_OWNER}/agile-infrastructure-build-client-scripts.git
+else
+    /usr/bin/git clone https://github.com/agile-deployer/agile-infrastructure-build-client-scripts.git
+fi
+
 cd agile-infrastructure-build-client-scripts
-#Start the build properly by calling the wrapper script. The user should then monitor the build on their new linode by tailing the logs in the logs directory
+
 /bin/sh HardcoreADTWrapper.sh
