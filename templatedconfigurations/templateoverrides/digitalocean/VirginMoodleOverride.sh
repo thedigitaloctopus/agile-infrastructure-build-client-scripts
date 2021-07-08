df4f4f425b5c633f6908e135f2d5860863007266
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-08 19:51:47 +0100

    Update VirginMoodleOverride.sh

--- templatedconfigurations/templateoverrides/digitalocean/VirginMoodleOverride.sh
+++ templatedconfigurations/templateoverrides/digitalocean/VirginMoodleOverride.sh
@@ -43,20 +43,64 @@ export SELECTED_TEMPLATE=\"5\" #Select a template number (1-10) to build you can
 
 . /root/Environment.env
 
-/bin/mkdir ~/.ssh
-/bin/echo "${SSH}" >> ~/.ssh/authorized_keys
+###############################################################################################
+# SET THESE FOR YOUR BUILD CLIENT MACHINE
+# THESE WILL BE THE USERNAME THE PASSWORD YOU CAN USE TO DO A SUDO ONCE AUTHENTICATED AND THE SSH PORT TO CONNECT ON
+# CONNECT TO YOUR BUILD CLIENT MACHINE AS FOLLOWS:
+# ssh -i <ssh-private-key> -p ${BUILDCLIENT_SSH_PORT} $BUILDCLIENT_USER@<buildclientip>
+# $BUILDCLIENT_USER>sudo su
+# password:${BUILDCLIENT_PASSWORD}
+# cd agile-infrastructure-build-client-scripts/logs
+# tail -f build*out*
+####################################################################################
+export BUILDCLIENT_USER="agile-deployer"
+export BUILDCLIENT_PASSWORD="mnbcxz098321QQZZ"
+export BUILDCLIENT_SSH_PORT="1035"
+
+/usr/sbin/adduser --disabled-password --gecos \"\" ${BUILDCLIENT_USER} 
+/bin/sed -i '$ a\ ClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 10000' /etc/ssh/sshd_config
+/bin/sed -i 's/.*PermitRootLogin.*$/PermitRootLogin no/g' /etc/ssh/sshd_config
+/bin/echo ${BUILDCLIENT_USER}:${BUILDCLIENT_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/chpasswd 
+ /usr/bin/gpasswd -a ${BUILDCLIENT_USER} sudo 
+
+/bin/mkdir -p /home/${BUILDCLIENT_USER}/.ssh
+/bin/echo "${SSH}" >> /home/${BUILDCLIENT_USER}/.ssh/authorized_keys
 
 /bin/sed -i 's/#*PasswordAuthentication [a-zA-Z]*/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+if ( [ "${BUILDCLIENT_SSH_PORT}" = "" ] )
+then
+    BUILDCLIENT_SSH_PORT="22"
+fi
+
+/bin/sed -i "s/^Port.*$/Port ${BUILDCLIENT_SSH_PORT}/g" /etc/ssh/sshd_config
+/bin/sed -i "s/^#Port.*$/Port ${BUILDCLIENT_SSH_PORT}/g" /etc/ssh/sshd_config
 
 systemctl restart sshd
 service ssh restart
 
 /usr/bin/apt -qq -y update
 /usr/bin/apt -qq -y install git
+/usr/bin/apt -qq -y install ufw
 
-cd /root
+/usr/sbin/ufw enable
+/usr/sbin/ufw default deny incoming
+/usr/sbin/ufw default allow outgoing
+####################################################################################################################################################
+#It is possible to lock down ssh connections to only from a specific ip address which is more secure, but, if the IP address of your machine changes,
+#for example, if you connect your laptop to a different network, then, you will have to connect to the build client machine through the console of
+#your VPS system provider and allow your new IP address through the firewall. This might be more of a hassle than its worth
+#####################################################################################################################################################
+/usr/sbin/ufw allow ${BUILDCLIENT_SSH_PORT}/tcp 
 
-/usr/bin/git clone https://github.com/agile-deployer/agile-infrastructure-build-client-scripts.git
+cd /home/${BUILDCLIENT_USER}
+
+if ( [ "${INFRASTRUCTURE_REPOSITORY_OWNER}" != "" ] )
+then
+    /usr/bin/git clone https://github.com/${INFRASTRUCTURE_REPOSITORY_OWNER}/agile-infrastructure-build-client-scripts.git
+else
+    /usr/bin/git clone https://github.com/agile-deployer/agile-infrastructure-build-client-scripts.git
+fi
 
 cd agile-infrastructure-build-client-scripts
 
