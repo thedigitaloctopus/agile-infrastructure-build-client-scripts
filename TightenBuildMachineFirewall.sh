61645fc7e7bb9167f867d7fa1b1f82968c0dfaef
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-24 15:02:34 +0100

    Update TightenBuildMachineFirewall.sh

--- TightenBuildMachineFirewall.sh
+++ TightenBuildMachineFirewall.sh
@@ -1,17 +1,15 @@
-/usr/bin/s3cmd get s3://adt/authorised-ips.dat
+/usr/bin/s3cmd --force get s3://adt/authorised-ips.dat
 
 if ( [ -f ${BUILD_HOME}/authorised-ips.dat ] )
 then
 
     /usr/sbin/ufw --force reset
-
+    /usr/sbin/ufw default deny incoming
+    /usr/sbin/ufw default allow outgoing
     while read ip
     do
-
         /usr/sbin/ufw allow from ${ip} to any port ${SSH_PORT}
-
     done < ${BUILD_HOME}/authorised-ips.dat
     
-    /usr/sbin/ufw enable
-
+    /bin/echo "y" | /usr/sbin/ufw enable
 fi
