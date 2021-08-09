f340d9c72a071e521e8a6ae7acf65f55bd110bcf
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-09 19:58:21 +0100

    Update SelectDNSProvider.sh

--- SelectDNSProvider.sh
+++ SelectDNSProvider.sh
@@ -27,11 +27,12 @@ status ""
 status "#####################################################################################################"
 status "#####Which DNS provider tech would you like to use for you website/application                  #####"
 status "#####We currently support 0: None 1: Cloudflare 2: Rackspace 3: Digital Ocean                   #####"
+status "#####                     4: Exoscale
 status "#####################################################################################################"
 status "Please select a DNS provider (0|1|2)"
 read choice
 
-while ( [ "${choice}" = "" ] || [ "`/bin/echo "0 1 2 3" | /bin/grep ${choice}`" = "" ] )
+while ( [ "${choice}" = "" ] || [ "`/bin/echo "0 1 2 3 4" | /bin/grep ${choice}`" = "" ] )
 do
     status "Invalid choice, please try again..."
     read choice
@@ -257,6 +258,60 @@ then
                 status "So, please input the access email address of your digital ocean account"
                 read DNS_USERNAME
                 /bin/echo "${DNS_USERNAME}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-digitalocean-credentials/DNSUSERNAME
+            fi
+        fi
+
+fi
+elif ( [ "${choice}" = "4" ] )
+then
+    DNS_CHOICE="exoscale"
+    /bin/mkdir -p ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials > /dev/null
+
+    if ( [ ! -f ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSSECURITYKEY ] )
+    then
+       status "#####################################################################################################"
+       status "We also need the access key for your exoscale account. You can either request this from the domain owner"
+       status "or if you are the domain owner, you can find it by authenticating to your digitalocean account, clicking "
+       status "on your name at the top right and then clicking on 'Account Settings and you should find the access key "
+       status "there and copy and paste it below"
+       status "NOTE: Your DNS access key is a composite of your two exoscale keys with dns access, the access key and the secret key"
+       status "Please enter these keys here as follows <exoscale dns access key>:<exoscale dns secret key>"
+       status "#####################################################################################################"
+       status "Please input your exoscale access key(s) - both access and secret separated with a colon"
+       read DNS_SECURITY_KEY
+       /bin/echo ${DNS_SECURITY_KEY} > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSSECURITYKEY
+       DNS_SECURITY_KEY="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSSECURITYKEY`"
+    else
+       DNS_SECURITY_KEY="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSSECURITYKEY`"
+       status "Have found an access key stored from a previous build for your exoscale account"
+       status "It is set to: ${DNS_SECURITY_KEY}"
+       status "Please enter Y/y if this is a correct access key"
+       read answer
+       if ( [ "`/bin/echo "${answer}" | /bin/grep 'y'`" = "" ]  && [ "`/bin/echo "${answer}" | /bin/grep 'Y'`" = "" ] )
+       then
+           status "So, please input the access key of your exoscale account"
+           read DNS_SECURITY_KEY
+           /bin/echo "${DNS_SECURITY_KEY}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSSECURITYKEY
+       fi
+   fi
+
+    if ( [ ! -f ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSUSERNAME ] )
+        then
+            status "Please input your Exoscale Email Address"
+            read DNS_USERNAME
+            /bin/echo ${DNS_USERNAME} > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSUSERNAME
+            DNS_USERNAME="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSUSERNAME`"
+        else
+            DNS_USERNAME="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSUSERNAME`"
+            status "Have found an email address stored from a previous build for your exoscale account"
+            status "It is set to: ${DNS_USERNAME}"
+            status "Please enter Y/y if this is a correct email address"
+            read answer
+            if ( [ "`/bin/echo "${answer}" | /bin/grep 'y'`" = "" ]  && [ "`/bin/echo "${answer}" | /bin/grep 'Y'`" = "" ] )
+            then
+                status "So, please input the access email address of your Exoscale account"
+                read DNS_USERNAME
+                /bin/echo "${DNS_USERNAME}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials/DNSUSERNAME
             fi
         fi
 
