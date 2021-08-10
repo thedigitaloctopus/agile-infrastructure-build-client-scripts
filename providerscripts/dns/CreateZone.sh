87a1915b030b2bdaf4832c92bf51f41763638437
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-10 13:30:55 +0100

    Update CreateZone.sh

--- providerscripts/dns/CreateZone.sh
+++ providerscripts/dns/CreateZone.sh
@@ -31,37 +31,37 @@ then
     /bin/echo "-1"
 else
 
-if ( [ "${dns}" = "cloudflare" ] )
-then
-    /usr/bin/curl -X POST "https://api.cloudflare.com/client/v4/zones" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${apikey}" -H "Content-Type: application/json" --data "{\"name\":\"${websiteurl}\"}" > /dev/null 2>&1
-fi
-
-email="${1}"
-apikey="${2}"
-websiteurl="`/bin/echo ${3} | /usr/bin/cut -d'.' -f2-`"
-dns="${4}"
-
-
-if ( [ "${dns}" = "exoscale" ] )
-then
-    /usr/bin/curl -H "X-DNS-Token: ${apikey}" -H 'Accept: application/json' -H 'Content-Type: application/json' -d "{\"domain\":{\"name\":\"${websiteurl}\"}}" -X POST https://api.exoscale.com/dns/v1/domains
-fi
-    
-    
-username="${1}"
-apikey="${2}"
-websiteurl="`/bin/echo ${3} | /usr/bin/cut -d'.' -f2-`"
-dns="${4}"
-region="${5}"
-
-if ( [ "${dns}" = "rackspace" ] )
-then
-    token="`/usr/bin/curl -s -X POST https://identity.api.rackspacecloud.com/v2.0/tokens -H "Content-Type: application/json" -d '{ "auth": { "RAX-KSKEY:apiKeyCredentials": { "username": "'${username}'", "apiKey": "'${apikey}'" } } }' | /usr/bin/python -m json.tool | /usr/bin/jq ".access.token.id" | /bin/sed 's/"//g'`"
-    if ( [ "${token}" = "null" ] )
+    if ( [ "${dns}" = "cloudflare" ] )
     then
-        /bin/echo "-1"
-    else
-        endpoint="`/usr/bin/curl -s -X POST https://identity.api.rackspacecloud.com/v2.0/tokens -H "Content-Type: application/json" -d '{ "auth": { "RAX-KSKEY:apiKeyCredentials": { "username": "'${username}'", "apiKey": "'${apikey}'" } } }' | /usr/bin/python -m json.tool | /usr/bin/jq ".access.serviceCatalog[].endpoints[].publicURL" | /bin/sed 's/"//g' | /bin/grep ${region} | /bin/grep dns`"
-        /usr/bin/curl -X POST ${endpoint}/domains -H "X-Auth-Token: ${token}" -H "Content-Type: application/json" -d '{ "domains" : [ { "name" : "'${websiteurl}'", "comment" : "Root level for '${websiteurl}'", "subdomains" : { "domains" : [] }, "ttl" : 300 , "emailAddress" : "webmaster@'${websiteurl}'" } ] }' | python -m json.tool
+        /usr/bin/curl -X POST "https://api.cloudflare.com/client/v4/zones" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${apikey}" -H "Content-Type: application/json" --data "{\"name\":\"${websiteurl}\"}" > /dev/null 2>&1
+    fi
+
+    email="${1}"
+    apikey="${2}"
+    websiteurl="`/bin/echo ${3} | /usr/bin/cut -d'.' -f2-`"
+    dns="${4}"
+
+    if ( [ "${dns}" = "exoscale" ] )
+    then
+        /usr/bin/curl -H "X-DNS-Token: ${apikey}" -H 'Accept: application/json' -H 'Content-Type: application/json' -d "{\"domain\":{\"name\":\"${websiteurl}\"}}" -X POST https://api.exoscale.com/dns/v1/domains
+    fi
+    
+    
+    username="${1}"
+    apikey="${2}"
+    websiteurl="`/bin/echo ${3} | /usr/bin/cut -d'.' -f2-`"
+    dns="${4}"
+    region="${5}"
+
+    if ( [ "${dns}" = "rackspace" ] )
+    then
+        token="`/usr/bin/curl -s -X POST https://identity.api.rackspacecloud.com/v2.0/tokens -H "Content-Type: application/json" -d '{ "auth": { "RAX-KSKEY:apiKeyCredentials": { "username": "'${username}'", "apiKey": "'${apikey}'" } } }' | /usr/bin/python -m json.tool | /usr/bin/jq ".access.token.id" | /bin/sed 's/"//g'`"
+        if ( [ "${token}" = "null" ] )
+        then
+            /bin/echo "-1"
+        else
+            endpoint="`/usr/bin/curl -s -X POST https://identity.api.rackspacecloud.com/v2.0/tokens -H "Content-Type: application/json" -d '{ "auth": { "RAX-KSKEY:apiKeyCredentials": { "username": "'${username}'", "apiKey": "'${apikey}'" } } }' | /usr/bin/python -m json.tool | /usr/bin/jq ".access.serviceCatalog[].endpoints[].publicURL" | /bin/sed 's/"//g' | /bin/grep ${region} | /bin/grep dns`"
+            /usr/bin/curl -X POST ${endpoint}/domains -H "X-Auth-Token: ${token}" -H "Content-Type: application/json" -d '{ "domains" : [ { "name" : "'${websiteurl}'", "comment" : "Root level for '${websiteurl}'", "subdomains" : { "domains" : [] }, "ttl" : 300 , "emailAddress" : "webmaster@'${websiteurl}'" } ] }' | python -m json.tool
+        fi
     fi
 fi
