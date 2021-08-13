13cf9f5ef08a30e4101eace7b4416dc2e9b2431d
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-13 18:52:57 +0100

    Update AddRecord.sh

--- providerscripts/dns/AddRecord.sh
+++ providerscripts/dns/AddRecord.sh
@@ -54,7 +54,7 @@ dns="${7}"
 
 if ( [ "${dns}" = "exoscale" ] )
 then
-    /usr/bin/curl  -H "X-DNS-Token: ${authkey}" -H 'Accept: application/json' -H 'Content-Type: application/json' -X POST -d "{\"record\":{\"name\": \"${subdomain}\",\"record_type\": \"A\",\"content\": \"${ip}\",\"ttl\": 3600}}" https://api.exoscale.com/dns/v1/domains/${domainurl}/records
+    /usr/bin/curl  -H "X-DNS-Token: ${authkey}" -H 'Accept: application/json' -H 'Content-Type: application/json' -X POST -d "{\"record\":{\"name\": \"${subdomain}\",\"record_type\": \"A\",\"content\": \"${ip}\",\"ttl\": 120}}" https://api.exoscale.com/dns/v1/domains/${domainurl}/records
 fi
 
 rootdomain="${1}"
