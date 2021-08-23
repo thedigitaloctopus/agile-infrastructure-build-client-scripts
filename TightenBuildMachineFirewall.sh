84e29dcce1de067e943b0ad90c2c6ad9ca7e4713
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-23 21:31:52 +0100

    Update TightenBuildMachineFirewall.sh

--- TightenBuildMachineFirewall.sh
+++ TightenBuildMachineFirewall.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+######################################################################################################
+# Description: By creating a file: s3://adt/authorised-ips.dat in your S3 style datastore with a list
+# of ipaddresses, you can allow only machines with your listed ip addresses to access your build machine.
+# The file authorised-ips.dat should be formatted with ip addresses on successive lines, for example:
+#
+# 111.111.111.111
+# 222.222.222.222
+#
+# Would allow machines with ip addresses 111.111.111.111 and 222.222.222.222 to connect to your build machine
+# Author: Peter Winter
+# Date: 17/01/2021
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
 
 if ( [ "`/usr/bin/crontab -l | /bin/grep Tighten`" = "" ] )
 then
