f78f24f0c415bc0ccc2871d23e5641ecb8386a76
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-02 23:30:51 +0100

    Update RegisterTemplateFromSnapshot.sh

--- providerscripts/server/RegisterTemplateFromSnapshot.sh
+++ providerscripts/server/RegisterTemplateFromSnapshot.sh
@@ -9,7 +9,7 @@ if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
     SNAPSHOT_ID=${snapshot_id}
     TEMPLATE_ID=$(/usr/bin/exo vm snapshot show --output-template {{.TemplateID}} ${SNAPSHOT_ID})
-    BOOTMODE=$(/usr/bin/exo exo vm template show --output-template {{.BootMode}} ${TEMPLATE_ID})
+    BOOTMODE=$(/usr/bin/exo vm template show --output-template {{.BootMode}} ${TEMPLATE_ID})
 fi
 
 if ( [ "${CLOUDHOST}" = "linode" ] )
