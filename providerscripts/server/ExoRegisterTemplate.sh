f7681becda44e76dabd4f1bead9f94079bf466bd
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-02 20:30:14 +0100

    Update and rename ExoRegisterTemplate.sh to RegisterTemplateFromSnapshot.sh

--- providerscripts/server/ExoRegisterTemplate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SNAPSHOT_ID=${snapshot_id}
-TEMPLATE_ID=$(exo vm snapshot show --output-template {{.TemplateID}} ${SNAPSHOT_ID})
-BOOTMODE=$(exo vm template show --output-template {{.BootMode}} ${TEMPLATE_ID})
---
+++ providerscripts/server/RegisterTemplateFromSnapshot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+then
+     :
+fi
+
+if ( [ "${CLOUDHOST}" = "exoscale" ] )
+then
+    SNAPSHOT_ID=${snapshot_id}
+    TEMPLATE_ID=$(/usr/bin/exo vm snapshot show --output-template {{.TemplateID}} ${SNAPSHOT_ID})
+    BOOTMODE=$(/usr/bin/exo exo vm template show --output-template {{.BootMode}} ${TEMPLATE_ID})
+fi
+
+if ( [ "${CLOUDHOST}" = "linode" ] )
+then
+     :
+fi
+
+if ( [ "${CLOUDHOST}" = "vultr" ] )
+then
+     :
+fi
+
+if ( [ "${CLOUDHOST}" = "aws" ] )
+then
+     :
+fi
