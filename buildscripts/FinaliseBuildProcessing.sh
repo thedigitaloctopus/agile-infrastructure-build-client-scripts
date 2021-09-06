0ded08e5f2683c0dd0f77a7354ff77a5c0197658
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-06 22:55:56 +0100

    Update FinaliseBuildProcessing.sh

--- buildscripts/FinaliseBuildProcessing.sh
+++ buildscripts/FinaliseBuildProcessing.sh
@@ -90,8 +90,8 @@ then
     /bin/cp ${BUILD_HOME}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER}.pub ${BUILD_HOME}/snapshots/${SERVER_USER}/keys/${CLOUDHOST}/${BUILD_IDENTIFIER}/
     /bin/echo "${AUTOSCALER_IMAGE_ID}:${WEBSERVER_IMAGE_ID}:${DATABASE_IMAGE_ID}" >> ${BUILD_HOME}/snapshots/${SERVER_USER}/snapshotIDs.dat
     /bin/mkdir -p ${BUILD_HOME}/snapshots/${SERVER_USER}/buildconfiguration/${CLOUDHOST}/
-    /bin/mkdir -p ${BUILD_HOME}/sshkeys
-    /bin/cp -r /root/.ssh/${SERVER_USER}.key ${BUILD_HOME}/sshkeys
+    /bin/mkdir -p ${BUILD_HOME}/snapshots/sshkeys
+    /bin/cp -r /root/.ssh/${SERVER_USER}.key ${BUILD_HOME}/snapshots/sshkeys
     /bin/cp -r ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-credentials ${BUILD_HOME}/snapshots/${SERVER_USER}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-credentials
     . ${BUILD_HOME}/providerscripts/datastore/PersistSnapshotData.sh
 fi
