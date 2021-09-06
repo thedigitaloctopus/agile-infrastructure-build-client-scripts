2c4226d53fb26a9be5dcbc637380e90df660df6f
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-06 15:20:39 +0100

    Update GetSnapshotIDs.sh

--- providerscripts/cloudhost/GetSnapshotIDs.sh
+++ providerscripts/cloudhost/GetSnapshotIDs.sh
@@ -31,6 +31,33 @@ fi
 
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
+    if ( [ "${REGION_ID}" = "1128bd56-b4d9-4ac6-a7b9-c715b187ce11" ] ) 
+    then 
+        region_id="ch-gva-2" 
+    fi 
+    if ( [ "${REGION_ID}" = "91e5e9e4-c9ed-4b76-bee4-427004b3baf9" ] ) 
+    then 
+        region_id="ch-dk-2" 
+    fi 
+    if ( [ "${REGION_ID}" = "4da1b188-dcd6-4ff5-b7fd-bde984055548" ] ) 
+    then 
+        region_id="at-vie-1" 
+    fi 
+    if ( [ "${REGION_ID}" = "35eb7739-d19e-45f7-a581-4687c54d6d02" ] ) 
+    then 
+        region_id="de-fra-1" 
+    fi 
+    if ( [ "${REGION_ID}" = "70e5f8b1-0b2c-4457-a5e0-88bcf1f3db68" ] ) 
+    then 
+        region_id="bg-sof-1" 
+    fi 
+    if ( [ "${REGION_ID}" = "85664334-0fd5-47bd-94a1-b4f40b1d2eb7" ] ) 
+    then 
+        region_id="de-muc-1" 
+    fi 
+    
+    zone=${region_id}
+    
     WEBSERVER_SNAPSHOT_NAME="`/usr/bin/exo -O json vm template list --mine --zone ${zone} | /usr/bin/jq --arg tmp_instance_name "${SNAPSHOT_ID}" '(.[] | select (.name | contains("webserver")  and  contains($tmp_instance_name)) | .name)' | /bin/sed 's/"//g'`"
     AUTOSCALER_IMAGE_ID="`/usr/bin/exo -O json vm template list --mine --zone ${zone} | /usr/bin/jq --arg tmp_instance_name "${SNAPSHOT_ID}" '(.[] | select (.name | contains("autoscaler")  and  contains($tmp_instance_name)) | .id)' | /bin/sed 's/"//g'`"
     WEBSERVER_IMAGE_ID="`/usr/bin/exo -O json vm template list --mine --zone ${zone} | /usr/bin/jq --arg tmp_instance_name "${SNAPSHOT_ID}" '(.[] | select (.name | contains("webserver")  and  contains($tmp_instance_name)) | .id)' | /bin/sed 's/"//g'`"
