39704ca328745c66c177a2789f47027059339958
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-07 11:26:45 +0100

    Update PersistBuildClientIP.sh

--- providerscripts/datastore/PersistBuildClientIP.sh
+++ providerscripts/datastore/PersistBuildClientIP.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 /usr/bin/s3cmd mb s3://adt-${BUILD_IDENTIFIER} 2>/dev/null
-/usr/bin/s3cmd --recursive --force del s3://adt-${BUILD_IDENTIFIER}/*
+/usr/bin/s3cmd ls s3://adt-${BUILD_IDENTIFIER}/  | /usr/bin/awk '{print $4}' | /usr/bin/xargs s3cmd del
 /bin/touch /tmp/${BUILD_CLIENT_IP}
 /usr/bin/s3cmd put /tmp/${BUILD_CLIENT_IP} s3://adt-${BUILD_IDENTIFIER}
