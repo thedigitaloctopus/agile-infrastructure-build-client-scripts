27a2482f46416472c98161a258cf4c6e66e22fdf
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-08 20:55:05 +0100

    Update BuildFromSnapshots.sh

--- buildscripts/BuildFromSnapshots.sh
+++ buildscripts/BuildFromSnapshots.sh
@@ -213,6 +213,7 @@ then
    # ASIPS="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "*autoscaler*" ${CLOUDHOST} | /bin/grep -P "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"`"
 
     ASIPS="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "*autoscaler*" ${CLOUDHOST} | /bin/grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | /bin/tr '\n' ':' | /bin/sed 's/\:$//g'`"
+    ASIP_PRIVATES="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "*autoscaler*" ${CLOUDHOST} | /bin/grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | /bin/tr '\n' ':' | /bin/sed 's/\:$//g'`"
     ASIPS_CLEANED="`/bin/echo ${ASIPS} | /bin/sed 's/\:/ /g'`"
     
     for ASIP in ${ASIPS_CLEANED}
