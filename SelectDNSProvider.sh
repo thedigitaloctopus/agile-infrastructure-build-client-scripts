ab4424ae6acd1f156b45675e4a92c73dee97a136
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-13 11:02:23 +0100

    Update SelectDNSProvider.sh

--- SelectDNSProvider.sh
+++ SelectDNSProvider.sh
@@ -26,8 +26,8 @@ status ""
 status ""
 status "#####################################################################################################"
 status "#####Which DNS provider tech would you like to use for you website/application                  #####"
-status "#####We currently support 0: None 1: Cloudflare 2: Rackspace 3: Digital Ocean                   #####"
-status "#####                     4: Exoscale                                                           #####"
+status "#####We currently support 0: None 1: Cloudflare 2: Digital Ocean                                #####"
+status "#####                     3: Exoscale                                                           #####"
 status "#####################################################################################################"
 status "Please select a DNS provider (0|1|2|3|4)"
 read choice
@@ -101,117 +101,6 @@ then
     ${BUILD_HOME}/providerscripts/dns/CreateZone.sh "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${WEBSITE_URL}" "${DNS_CHOICE}" "${DNS_REGION}"
 elif ( [ "${choice}" = "2" ] )
 then
-    while ( [ "`${BUILD_HOME}/providerscripts/dns/CreateZone.sh "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${WEBSITE_URL}" "${DNS_CHOICE}" "${DNS_REGION}"`" = "-1" ] )
-    do
-        status "Can't find valid credentials to authenticate to rackspace. Please enter or modify the credentials you have supplied"
-        /bin/mkdir -p ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials
-        if ( [ ! -f ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME ] )
-        then
-            status "Please enter your Rackspace username"
-            read DNS_USERNAME
-            /bin/echo ${DNS_USERNAME} > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME
-        else
-            DNS_USERNAME="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME`"
-            status "Have found a username stored from a previous build for your Rackspace account"
-            status "It is set to: ${DNS_USERNAME}"
-            status "Please enter Y/y if this is a correct username"
-            read answer
-            if ( [ "`/bin/echo "${answer}" | /bin/grep 'y'`" = "" ]  && [ "`/bin/echo "${answer}" | /bin/grep 'Y'`" = "" ] )
-            then
-                status "So, please input the username of your rackspace account"
-                read DNS_USERNAME
-                /bin/echo "${DNS_USERNAME}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME
-            fi
-        fi
-
-        if ( [ ! -f ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSSECURITYKEY ] )
-        then
-            status "#####################################################################################################"
-            status "We also need the API key for your rackspace account. You can either request this from the domain owner"
-            status "or if you are the domain owner, you can find it by authenticating to your rackspace account, clicking "
-            status "on your name at the top right and then clicking on 'Account Settings and you should find the API key "
-            status "there and copy and paste it below"
-            status "#####################################################################################################"
-            status "Please input your rackspace API key"
-            read DNS_SECURITY_KEY
-            /bin/echo ${DNS_SECURITY_KEY} > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSSECURITYKEY
-            DNS_SECURITY_KEY="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSSECURITYKEY`"
-        else
-            DNS_SECURITY_KEY="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSSECURITYKEY`"
-            status "Have found an access key stored from a previous build for your rackspace account"
-            status "It is set to: ${DNS_SECURITY_KEY}"
-            status "Please enter Y/y if this is a correct access key"
-            read answer
-            if ( [ "`/bin/echo "${answer}" | /bin/grep 'y'`" = "" ]  && [ "`/bin/echo "${answer}" | /bin/grep 'Y'`" = "" ] )
-            then
-                status "So, please input the access key of your rackspace account"
-                read DNS_SECURITY_KEY
-                /bin/echo "${DNS_SECURITY_KEY}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSSECURITYKEY
-            fi
-        fi
-
-        if ( [ ! -f ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME ] )
-        then
-            status "Please input your rackspace Email Address"
-            read DNS_USERNAME
-            /bin/echo ${DNS_USERNAME} > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME
-            DNS_USERNAME="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME`"
-        else
-            DNS_USERNAME="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME`"
-            status "Have found an email address stored from a previous build for your rackspace account"
-            status "It is set to: ${DNS_USERNAME}"
-            status "Please enter Y/y if this is a correct email address"
-            read answer
-            if ( [ "`/bin/echo "${answer}" | /bin/grep 'y'`" = "" ]  && [ "`/bin/echo "${answer}" | /bin/grep 'Y'`" = "" ] )
-            then
-                status "So, please input the access email address of your rackspace account"
-                read DNS_USERNAME
-                /bin/echo "${DNS_USERNAME}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSUSERNAME
-            fi
-        fi
-        if ( [ ! -f ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSREGION ] )
-        then
-            answer=""
-            DNS_REGION=""
-            while ( [ "${DNS_REGION}" = "" ] )
-            do
-                status "Please enter the three letter identifier for the region you wish to deploy to from the following list"
-                status "Dallas - DFW, Chicago - ORD, Virginia IAD, London LON, Sydney SYD, Hong Kon HKG"
-                read answer
-                if ( [ "`/bin/echo "DFW ORD IAD LON SYD HKG" | /bin/grep ${answer}`" != "" ] )
-                then
-                    DNS_REGION="`/bin/echo ${answer} | /usr/bin/tr '[:upper:]' '[:lower:]'`"
-                    /bin/echo "${DNS_REGION}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSREGION
-                fi
-            done
-        else
-            DNS_REGION="`/bin/cat ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSREGION`"
-            status "Have found an selected region stored from a previous build for your rackspace web dns account"
-            status "It is set to: ${DNS_REGION}"
-            status "Please enter Y/y if this is a correct region"
-            read answer
-            if ( [ "`/bin/echo "${answer}" | /bin/grep 'y'`" = "" ]  && [ "`/bin/echo "${answer}" | /bin/grep 'Y'`" = "" ] )
-            then
-                answer=""
-                DNS_REGION=""
-                while ( [ "${DNS_REGION}" = "" ] )
-                do
-                    status "Please enter the three letter identifier for the region you wish to deploy to from the following list"
-                    status "Dallas - DFW, Chicago - ORD, Virginia IAD, London LON, Sydney SYD, Hong Kon HKG"
-                    read answer
-                    if ( [ "`/bin/echo "DFW ORD IAD LON SYD HKG" | /bin/grep ${answer}`" != "" ] )
-                    then
-                        DNS_REGION="`/bin/echo ${answer} | /usr/bin/tr '[:upper:]' '[:lower:]'`"
-                        /bin/echo "${DNS_REGION}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSREGION
-                    fi
-                done
-            fi
-        fi
-        DNS_CHOICE="rackspace"
-        /bin/echo ${DNS_CHOICE} > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-rackspace-credentials/DNSCHOICE
-    done
-elif ( [ "${choice}" = "3" ] )
-then
     DNS_CHOICE="digitalocean"
     /bin/mkdir -p ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-digitalocean-credentials > /dev/null
 
@@ -261,7 +150,7 @@ then
            /bin/echo "${DNS_USERNAME}" > ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-digitalocean-credentials/DNSUSERNAME
        fi
    fi
-elif ( [ "${choice}" = "4" ] )
+elif ( [ "${choice}" = "3" ] )
 then
     DNS_CHOICE="exoscale"
     /bin/mkdir -p ${BUILD_HOME}/buildconfiguration/${CLOUDHOST}/${BUILD_IDENTIFIER}-exoscale-credentials > /dev/null
