6ed07826a4527e3443039b82a77ddd1484609ebe
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-15 18:48:04 +0100

    Update GetAllRecordIDs.sh

--- providerscripts/dns/GetAllRecordIDs.sh
+++ providerscripts/dns/GetAllRecordIDs.sh
@@ -49,7 +49,7 @@ dns="${5}"
 
 if ( [ "${dns}" = "exoscale" ] )
 then
-    /usr/bin/exo -O json dns list | /usr/bin/jq --arg tmp_subdomain "${subdomain}" '.[].record | select (.name == $tmp_subdomain) | .id'
+    /usr/bin/exo -O json dns list | /usr/bin/jq --arg tmp_domain "${domainurl}" '.[] | select (.name == $tmp_domain) | .id'    
     #Alternative
     #/usr/bin/curl  -H "X-DNS-Token: ${authkey}" -H 'Accept: application/json' https://api.exoscale.com/dns/v1/domains/${domainurl}/records | /usr/bin/jq --arg tmp_subdomain "${subdomain}" '.[].record | select (.name == $tmp_subdomain) | .id'
 fi
