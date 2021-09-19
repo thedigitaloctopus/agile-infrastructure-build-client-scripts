f5654ab8db8040d567e16d29504ae0361f97b6c4
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-19 22:26:17 +0100

    Create BackupBuildMachine.sh

---
+++ BackupBuildMachine.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+######################################################################################################
+# Description: By creating a file: s3://backup-${BUILD_IDENTIFIER}/authorised-ips.dat this will backup your
+# build machine on a daily basis
+# Author: Peter Winter
+# Date: 17/01/2021
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+
+if ( [ "`/usr/bin/crontab -l | /bin/grep BackupBuildMachine`" = "" ] )
+then
+    /bin/echo "@daily ${BUILD_HOME}/BackupBuildMachine.sh ${BUILD_IDENTIFIER}" >> /var/spool/cron/crontabs/root
+    /usr/bin/crontab -u root /var/spool/cron/crontabs/root
+fi
+
+if ( [ "${1}" != "" ] )
+then
+    BUILD_IDENTIFIER="${1}"
+fi
+
+backupno="`/usr/bin/s3cmd ls s3://backup-${BUILD_IDENTIFIER} | /usr/bin/wc -l`"
+
+/bin/tar -cfvz /tmp/backup-${BUILD_IDENTIFIER}-${backupno}.tar.gz ${BUILD_HOME}/*
+
+/usr/bin/s3cmd put /tmp/backup-${BUILD_IDENTIFIER}.tar.gz s3://backup-${BUILD_IDENTIFIER}
+
+/bin/rm /tmp/backup-${BUILD_IDENTIFIER}-${backupno}.tar.gz
+
