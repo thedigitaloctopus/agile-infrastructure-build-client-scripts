48ca5f4a69d634cf589cbfccfab0f21a038a3040
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-24 00:12:04 +0100

    Update BuildWebserver.sh

--- buildscripts/BuildWebserver.sh
+++ buildscripts/BuildWebserver.sh
@@ -274,12 +274,12 @@ do
         #We are to have any chance of getting a working autoscaler, then we must install git on our machine as it is not bundled
         #or available be default, so, lets do that
 
-        /usr/bin/ssh ${OPTIONS} ${SERVER_USER}@${ip} "DEBIAN_FRONTEND=noninteractive /bin/sh -c '/bin/touch /home/${SERVER_USER}/${MACHINE_TYPE} ; ${CUSTOM_USER_SUDO} /usr/bin/apt-get -qq -y update && /usr/bin/apt-get install -qq git ; cd /home/${SERVER_USER} ; /usr/bin/git init ; /bin/mkdir /home/${SERVER_USER}/bootstrap ; ${CUSTOM_USER_SUDO}  /usr/bin/git config --global init.defaultBranch master ; ${CUSTOM_USER_SUDO} /usr/bin/git config --global pull.rebase false'"
+        /usr/bin/ssh ${OPTIONS} ${SERVER_USER}@${ip} "DEBIAN_FRONTEND=noninteractive /bin/sh -c '/bin/touch /home/${SERVER_USER}/${MACHINE_TYPE} ; ${CUSTOM_USER_SUDO} /usr/bin/apt-get -qq -y update; ${CUSTOM_USER_SUDO}  /usr/bin/apt-get install -qq -y git ; cd /home/${SERVER_USER} ; /usr/bin/git init ; /bin/mkdir /home/${SERVER_USER}/bootstrap ; ${CUSTOM_USER_SUDO}  /usr/bin/git config --global init.defaultBranch master ; ${CUSTOM_USER_SUDO} /usr/bin/git config --global pull.rebase false'"
         
         while ( [ "$?" != "0" ] )
         do 
             /bin/sleep 10
-            /usr/bin/ssh ${OPTIONS} ${SERVER_USER}@${ip} "DEBIAN_FRONTEND=noninteractive /bin/sh -c '/bin/touch /home/${SERVER_USER}/${MACHINE_TYPE} ; ${CUSTOM_USER_SUDO} /usr/bin/apt-get install -qq git ; cd /home/${SERVER_USER} ; /usr/bin/git init ; /bin/mkdir /home/${SERVER_USER}/bootstrap ; ${CUSTOM_USER_SUDO}  /usr/bin/git config --global init.defaultBranch master ; ${CUSTOM_USER_SUDO} /usr/bin/git config --global pull.rebase false'"
+            /usr/bin/ssh ${OPTIONS} ${SERVER_USER}@${ip} "DEBIAN_FRONTEND=noninteractive /bin/sh -c '/bin/touch /home/${SERVER_USER}/${MACHINE_TYPE} ; ${CUSTOM_USER_SUDO} /usr/bin/apt-get -qq -y update; ${CUSTOM_USER_SUDO} /usr/bin/apt-get install -qq -y git ; cd /home/${SERVER_USER} ; /usr/bin/git init ; /bin/mkdir /home/${SERVER_USER}/bootstrap ; ${CUSTOM_USER_SUDO}  /usr/bin/git config --global init.defaultBranch master ; ${CUSTOM_USER_SUDO} /usr/bin/git config --global pull.rebase false'"
         done
 
         /usr/bin/scp ${OPTIONS} ${BUILD_HOME}/providerscripts/git/GitPull.sh ${BUILD_HOME}/providerscripts/git/GitFetch.sh ${BUILD_HOME}/providerscripts/git/GitCheckout.sh ${SERVER_USER}@${ip}:/home/${SERVER_USER}/bootstrap
