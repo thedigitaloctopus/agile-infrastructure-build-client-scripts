060e8bb59d8f46f00d9e4b425efd54c7ada2226e
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-01 20:28:40 +0000

    Update WhichApplicationByGitAndBackup.sh

--- providerscripts/application/WhichApplicationByGitAndBackup.sh
+++ providerscripts/application/WhichApplicationByGitAndBackup.sh
@@ -25,17 +25,6 @@ status () {
     /bin/echo "$1" | /usr/bin/tee /dev/fd/3
 }
 
-
-database_engine="`/bin/cat ${INTERROGATION_HOME}/${backuprepository}/dbe.dat`"
-if ( [ "${database_engine}" != "" ] && [ "${database_engine}" != "${DATABASE_INSTALLATION_TYPE}" ] )
-then 
-    if ( [ "${database_engine}" = "MySQL" ] && [ "${DATABASE_INSTALLATION_TYPE}" = "Maria" ] )
-    then
-        database_engine="Maria"
-    fi
-    export DATABASE_INSTALLATION_TYPE="${database_engine}"
-fi
-
 #################JOOMLA################
 if ( [ -d ${INTERROGATION_HOME}/${backuprepository}/administrator ] && [ -d ${INTERROGATION_HOME}/${backuprepository}/modules ] && [ -d ${INTERROGATION_HOME}/${backuprepository}/plugins ] )
 then
