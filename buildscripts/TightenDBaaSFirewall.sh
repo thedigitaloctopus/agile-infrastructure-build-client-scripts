012bcd7df5a351c871d35118eaf0512651fbd066
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-12 10:11:24 +0000

    Update TightenDBaaSFirewall.sh

--- buildscripts/TightenDBaaSFirewall.sh
+++ buildscripts/TightenDBaaSFirewall.sh
@@ -3,5 +3,5 @@
 if ( [ "${CLOUDHOST}" = "digitalocean" ] && [ "${DATABASE_INSTALLATION_TYPE}"="DBaaS" ] )
 then
    status "Tightening the firewall on your database cluster for your webserver"    
-   /usr/local/bin/doctl databases firewalls append ${cluster_id} --rule ip_addr:${BUILD_CLIENT_IP}  
+   /usr/local/bin/doctl databases firewalls append ${cluster_id} --rule ip_addr:${WSIP}  
 fi
