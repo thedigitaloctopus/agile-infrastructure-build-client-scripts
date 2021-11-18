28433cc8fdfe64c58b2fe0d3f081f92cde7320fd
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-18 16:35:44 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -424,6 +424,7 @@ status ""
 . ${BUILD_HOME}/TightenBuildMachineFirewall.sh
 . ${BUILD_HOME}/BackupBuildMachine.sh
 . ${BUILD_HOME}/providerscripts/cloudhost/ValidateProviderAuthorisation.sh
+. ${BUILD_HOME}/providerscripts/cloudhost/SetupAdditionalCloudhostTools.sh
 . ${BUILD_HOME}/providerscripts/datastore/PersistBuildClientIP.sh
 
 
