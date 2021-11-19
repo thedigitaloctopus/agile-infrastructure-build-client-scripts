9367560ea9ca0fa521da45c822e602574046f34a
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-19 15:00:39 +0000

    Update TightenDBaaSFirewall.sh

--- buildscripts/TightenDBaaSFirewall.sh
+++ buildscripts/TightenDBaaSFirewall.sh
@@ -15,3 +15,21 @@ then
    /usr/local/bin/doctl databases firewalls append ${cluster_id} --rule ip_addr:${BUILD_CLIENT_IP}  
    
 fi
+
+if ( [ "${CLOUDHOST}" = "exoscale" ] && [ "${DATABASE_INSTALLATION_TYPE}"="DBaaS" ] )
+then
+   if ( [ "${ASIP}" != "" ] )
+   then
+       ips="${ASIP},${WSIP},${DBIP},${BUILD_CLIENT_IP}""
+   else
+       ips="${WSIP},${DBIP},${BUILD_CLIENT_IP}""
+   fi
+
+    if ( [ "${DATABASE_ENGINE}" = "pg" ] )
+    then
+        /usr/bin/exo dbaas update -z ${DATABASE_REGION} ${DBaaS_DBNAME} --pg-ip-filter=${ips}
+    elif ( [ "${DATABASE_ENGINE}" = "mysql" ] )
+    then
+        /usr/bin/exo dbaas update -z ${DATABASE_REGION} ${DBaaS_DBNAME} --mysql-ip-filter=${ips}
+    fi
+fi
