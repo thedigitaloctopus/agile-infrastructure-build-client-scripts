5bc7f9c1aeaba8858fe28e112ded3af6f1e7362e
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-24 16:42:16 +0000

    Update ManuallyUpdateSSLCertificate.sh

--- helperscripts/ManuallyUpdateSSLCertificate.sh
+++ helperscripts/ManuallyUpdateSSLCertificate.sh
@@ -1,10 +1,26 @@
-###################################################################################################################################################
-# Description: If you have bought your own ssl certificate from a 3rd party, then you can use this script to update your chain file and your private
-# key when that certificate expires. Typically a certificate is valid for a period of 1 year and up.
+#!/bin/bash
+########################################################################################
 # Author: Peter Winter
-# Date: 30/04/2017
-###################################################################################################################################################
-#!/bin/sh
+# Date  : 12/07/2016
+# Description: If you have bought your own ssl certificate from a 3rd party, then you can use 
+# this script to update your chain file and your private key when that certificate expires. 
+# Typically a certificate is valid for a period of 1 year and up.
+########################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################################
+#########################################################################################
+#set -x
 
 if ( [ ! -f  ./ManuallyUpdateSSLCertificate.sh ] )
 then
