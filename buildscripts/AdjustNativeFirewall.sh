c7bcb562013a5a1947148256013536479d5ac502
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-12-05 20:27:23 +0000

    Update AdjustNativeFirewall.sh

--- buildscripts/AdjustNativeFirewall.sh
+++ buildscripts/AdjustNativeFirewall.sh
@@ -4,10 +4,12 @@
 
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
+    /bin/echo "y" | /usr/bin/exo compute security-group delete adt-build-machine
+    /usr/bin/exo compute security-group create adt-build-machine
     if ( [ "${ip}" != "NOIP" ] )
     then
-        /usr/bin/exo compute security-group rule add adt --network ${ip}/32 --port ${SSH_PORT}
+        /usr/bin/exo compute security-group rule add adt-build-machine --network ${ip}/32 --port ${SSH_PORT}
     else
-        /usr/bin/exo compute security-group rule add adt --network 0.0.0.0/0 --port ${SSH_PORT}
+        /usr/bin/exo compute security-group rule add adt-build-machine --network 0.0.0.0/0 --port ${SSH_PORT}
     fi
 fi
