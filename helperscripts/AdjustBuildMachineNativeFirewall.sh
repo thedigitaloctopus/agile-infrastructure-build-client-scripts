87efa8541c53051cfff5e0f6f9d4213dfbf072bb
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-12-06 20:17:42 +0000

    Update AdjustBuildMachineNativeFirewall.sh

--- helperscripts/AdjustBuildMachineNativeFirewall.sh
+++ helperscripts/AdjustBuildMachineNativeFirewall.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
-
-
+########################################################################################################
+# Author : Peter Winter
+# Date   : 13/07/2016
+# Description : You can use this script to authorise new laptop ip addresses to access your build machine
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
 if ( [ "${1}" = "" ] || [ "${2}" = "" ] || [ "${3}" = "" ] )
 then
     /bin/echo "Sorry usage: ${0} <build-identifier> <ip> <mode - add|remove>"
