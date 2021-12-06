220b7ce9035d8bed232dda41d85dd5a9422147eb
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-12-06 22:34:08 +0000

    Update AdjustBuildMachineNativeFirewall.sh

--- buildscripts/AdjustBuildMachineNativeFirewall.sh
+++ buildscripts/AdjustBuildMachineNativeFirewall.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-
-
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
-    /bin/echo "y" | /usr/bin/exo compute security-group delete adt-build-machine
-    /usr/bin/exo compute security-group create adt-build-machine
+    
+    if ( [ "`/usr/bin/exo -O json compute security-group list adt-build-machine | /usr/bin/jq '.[] | select (.name == "adt-build-machine")'`" = "" ] )
+    then
+        /usr/bin/exo compute security-group create adt-build-machine
+    fi
+    
     if ( [ "${ip}" != "NOIP" ] )
     then
-        /usr/bin/exo compute security-group rule add adt-build-machine --network ${ip}/32 --port ${SSH_PORT}
-        /usr/bin/exo compute security-group rule add adt --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
+        /usr/bin/exo compute security-group rule add adt-build-machine --network ${ip}/32 --port ${SSH_PORT} 2>/dev/null
+        /usr/bin/exo compute security-group rule add adt-build-machine --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8 2>/dev/null
     else
-        /usr/bin/exo compute security-group rule add adt-build-machine --network 0.0.0.0/0 --port ${SSH_PORT}
-        /usr/bin/exo compute security-group rule add adt --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
+        /usr/bin/exo compute security-group rule add adt-build-machine --network 0.0.0.0/0 --port ${SSH_PORT} 2>/dev/null
+        /usr/bin/exo compute security-group rule add adt-build-machine --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8 2>/dev/null
     fi
+    
 fi
