1b7b4e4cf55478aebbb30df43c2a7c15d5fa1f38
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-12-06 15:56:04 +0000

    Update MonitorWebserver.sh

--- helperscripts/MonitorWebserver.sh
+++ helperscripts/MonitorWebserver.sh
@@ -32,7 +32,7 @@ then
     exit
 fi
 
-export BUILD_HOME="`/usr/bin/pwd | /bin/sed 's/\/helper.*//g'`"
+export BUILD_HOME="`/bin/pwd | /bin/sed 's/\/helper.*//g'`"
 
 if ( [ ! -d ${BUILD_HOME}/helperscripts/logs ] )
 then
