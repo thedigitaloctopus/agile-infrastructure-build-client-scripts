62e4a84f6ddaedbbc7b698b769ea869c0ba7a7d9
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-12-13 16:43:16 +0000

    Update AdjustBuildMachineNativeFirewall.sh

--- buildscripts/AdjustBuildMachineNativeFirewall.sh
+++ buildscripts/AdjustBuildMachineNativeFirewall.sh
@@ -21,9 +21,14 @@ fi
 
 if ( [ "${CLOUDHOST}" = "linode" ] )
 then
-    if ( [ "`/usr/local/bin/linode-cli --json firewalls list | jq '.[] | select (.label == "adt-build-machine" ).id'`" = "" ] )
+    firewall_id="`/usr/local/bin/linode-cli --json firewalls list | jq '.[] | select (.label == "adt-build-machine" ).id'`"
+
+    if ( [ "${firewall_id}" = "" ] )
     then
         /usr/local/bin/linode-cli firewalls create --label "adt-build-machine" --rules.inbound_policy DROP   --rules.outbound_policy ACCEPT
+    else
+       /usr/local/bin/linode-cli firewalls delete ${firewall_id}
+       /usr/local/bin/linode-cli firewalls create --label "adt-build-machine" --rules.inbound_policy DROP   --rules.outbound_policy ACCEPT
     fi
     
     firewall_id="`/usr/local/bin/linode-cli --json firewalls list | jq '.[] | select (.label == "adt-build-machine" ).id'`"
@@ -34,21 +39,24 @@ then
         if ( [ "${ips}" != "" ] )
         then
             ips="`/bin/echo ${ips} | /bin/sed 's/:/ /g'`"
-            for listedip in ${ips}
-            do
-                iplist="${iplist}[\"${listedip}/32\"],"
-            done
-            iplist="`/bin/echo ${iplist} | /bin/sed 's/,$//g'`"
         fi
-        if ( [ "${iplist}" = "" ] )
+        if ( [ "${ips}" = "" ] )
         then
             /usr/local/bin/linode-cli firewalls rules-update --inbound  "[{\"addresses\":{\"ipv4\":[\"${ip}/32\"]},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT}\"}]" ${firewall_id}
         else
-            /usr/local/bin/linode-cli firewalls rules-update --inbound  "[{\"addresses\":{\"ipv4\":${iplist}},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT}\"}]" ${firewall_id}
+            rules=""
+            for ipaddress in ${ips}
+            do
+                 rules=$rules"{\"addresses\":{\"ipv4\":[\"${ipaddress}/32\"]},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT}\"},"
+            done
+            rules="`/bin/echo ${rules} | /bin/sed 's/,$//g'`"
+            /usr/local/bin/linode-cli firewalls rules-update --inbound  "[${rules}]" ${firewall_id}
+            #/usr/local/bin/linode-cli firewalls rules-update --inbound  "[{\"addresses\":{\"ipv4\":${iplist}},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT}\"}]" ${firewall_id}
         fi
     else
         /usr/local/bin/linode-cli firewalls rules-update --inbound  "[{\"addresses\":{\"ipv4\":[\"0.0.0.0/0\"]},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT}\"},{\"addresses\":{\"ipv4\":[\"0.0.0.0/0\"]},\"action\":\"ACCEPT\",\"protocol\":\"ICMP\"}]" ${firewall_id}       
     fi
+
      bmip="`/usr/bin/wget http://ipinfo.io/ip -qO -`"
      bmid="`/usr/local/bin/linode-cli --json linodes list | jq --arg tmp_ip "${bmip}" '.[] | select (.ipv4 | tostring | contains ($tmp_ip))'.id | /bin/sed 's/\"//g'`"
      
