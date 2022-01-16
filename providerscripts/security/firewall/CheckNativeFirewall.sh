2e7011afa3e9a31c07e949008c8be65cab735f96
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-16 21:26:11 +0000

    Update CheckNativeFirewall.sh

--- providerscripts/security/firewall/CheckNativeFirewall.sh
+++ providerscripts/security/firewall/CheckNativeFirewall.sh
@@ -360,7 +360,10 @@ then
        do              
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${machine_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${DB_PORT} --protocol tcp --size 32 --type v4 -s ${machine_ip}
-           /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_ip}
+           if ( [ "`/bin/ls ${HOME}/config/beingbuiltpublicips | /bin/grep ${machine_ip}`" != "" ] )
+           then
+               /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_ip}
+           fi
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${BUILD_CLIENT_IP}
        done
        
@@ -373,6 +376,10 @@ then
        do              
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${DB_PORT} --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
+           if ( [ "`/bin/ls ${HOME}/config/beingbuiltips | /bin/grep ${machine_private_ip}`" != "" ] )
+           then
+               /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
+           fi
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${BUILD_CLIENT_IP}
        done
