70c97c792d2333c5772e14ba2ef29c499ca79ecb
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-21 12:46:38 +0000

    Update CheckNativeFirewall.sh

--- providerscripts/security/firewall/CheckNativeFirewall.sh
+++ providerscripts/security/firewall/CheckNativeFirewall.sh
@@ -378,7 +378,7 @@ then
        do              
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${DB_PORT} --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
-           /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
+      #     /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${BUILD_CLIENT_IP}
        done
        
