80991ffc0b189deb6f770ef7585b75714109ec28
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-03 19:37:00 +0000

    Update CreateZone.sh

--- providerscripts/dns/CreateZone.sh
+++ providerscripts/dns/CreateZone.sh
@@ -82,13 +82,4 @@ else
         /usr/bin/vultr dns domain create -d ${domainurl}
     fi
     
-    email="${1}"
-    authkey="${2}"
-    domainurl="`/bin/echo ${3} | /usr/bin/cut -d'.' -f2-`"
-    dns="${4}"
-    
-    if ( [ "${dns}" = "aws" ] )
-    then
-        /usr/local/bin/cli53 create ${domainurl}
-    fi
 fi
