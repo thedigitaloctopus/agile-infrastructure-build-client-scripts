411b509b538506ff27c9bdc24cb1b03b21d3a4b3
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-07 13:30:16 +0000

    Update GetServerIPAddresses.sh

--- providerscripts/server/GetServerIPAddresses.sh
+++ providerscripts/server/GetServerIPAddresses.sh
@@ -61,7 +61,7 @@ fi
 
 if ( [ "${cloudhost}" = "aws" ] )
 then
-    /usr/bin/aws ec2 describe-instances --filter "Name=tag:descriptiveName,Values=${server_type}*" "Name=instance-state-name,Values=running" | /usr/bin/jq '.Reservations[].Instances[].PublicIpAddress' | /bin/sed 's/\"//g'
+    /usr/bin/aws ec2 describe-instances --filter "Name=tag:descriptiveName,Values=*${server_type}*" "Name=instance-state-name,Values=running" | /usr/bin/jq '.Reservations[].Instances[].PublicIpAddress' | /bin/sed 's/\"//g'
 fi
 
 
