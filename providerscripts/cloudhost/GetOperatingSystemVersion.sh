52beb8d96cf971f39437b2fb8e864187663db622
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-08 18:26:48 +0000

    Update GetOperatingSystemVersion.sh

--- providerscripts/cloudhost/GetOperatingSystemVersion.sh
+++ providerscripts/cloudhost/GetOperatingSystemVersion.sh
@@ -101,9 +101,9 @@ elif ( [ "${buildos}" = "debian" ] )
     then
         if ( [ "${buildosversion}" = "10" ] )
         then
-            /bin/echo "     AMI                  |          OS" >&3
+            /bin/echo "     AMI                            OS" >&3
             /bin/echo "=====================================================" >&3
-            /usr/bin/aws ec2 describe-images --owners 136693071363 | /usr/bin/jq '.Images[] | .ImageId + "     " + .Name' | /bin/grep debian-10-amd64 | /bin/grep "2022\|2023\|2024\|2025\|2026" | /bin/sed 's/"//g' >&3
+            /usr/bin/aws ec2 describe-images --owners 136693071363 | /usr/bin/jq '.Images[] | .ImageId + "  |   " + .Name' | /bin/grep debian-10-amd64 | /bin/grep "2022\|2023\|2024\|2025\|2026" | /bin/sed 's/"//g' >&3
             /bin/echo "Please enter the ami identifier for the OS you wish to use" >&3
             read ami_identifier  
             while ( [ "${ami_identifier}" = "" ] )
@@ -116,9 +116,9 @@ elif ( [ "${buildos}" = "debian" ] )
         fi
         if ( [ "${buildosversion}" = "11" ] )
         then
-            /bin/echo "     AMI                   |          OS" >&3
+            /bin/echo "     AMI                             OS" >&3
             /bin/echo "======================================================" >&3
-            /usr/bin/aws ec2 describe-images --owners 136693071363 | /usr/bin/jq '.Images[] | .ImageId + "     " + .Name' | /bin/grep debian-11-amd64 | /bin/grep "2022\|2023\|2024\|2025\|2026" | /bin/sed 's/"//g' >&3
+            /usr/bin/aws ec2 describe-images --owners 136693071363 | /usr/bin/jq '.Images[] | .ImageId + "  |   " + .Name' | /bin/grep debian-11-amd64 | /bin/grep "2022\|2023\|2024\|2025\|2026" | /bin/sed 's/"//g' >&3
             /bin/echo "Please enter the ami identifier for the OS you wish to use" >&3
             read ami_identifier          
             while ( [ "${ami_identifier}" = "" ] )
