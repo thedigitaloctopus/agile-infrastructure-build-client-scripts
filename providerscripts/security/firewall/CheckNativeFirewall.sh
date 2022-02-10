6397bc055d65036210b8142e8de1b71cf91f616f
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-10 18:33:59 +0000

    Update CheckNativeFirewall.sh

--- providerscripts/security/firewall/CheckNativeFirewall.sh
+++ providerscripts/security/firewall/CheckNativeFirewall.sh
@@ -488,6 +488,14 @@ then
        do
             /usr/bin/aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --protocol tcp  --port 22 --cidr ${autoscaler_ip}/32
        done
+       
+       if ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] )
+       then
+           for webserver_ip in ${webserver_ips}
+           do
+               /usr/bin/aws rds authorize-db-security-group-ingress --db-security-group-name "agiledeploymenttoolkitdbsecuritygroup" --cidr ${webserver_ip}/32
+           done
+       fi
               
         autoscaler_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh autoscaler ${CLOUDHOST}`"
         webserver_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh webserver ${CLOUDHOST}`"
@@ -504,6 +512,14 @@ then
        do
             /usr/bin/aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --protocol tcp  --port 22 --cidr ${autoscaler_private_ip}/32
        done
+       
+       if ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] )
+       then
+           for webserver_private_ip in ${webserver_private_ips}
+           do
+               /usr/bin/aws rds authorize-db-security-group-ingress --db-security-group-name "agiledeploymenttoolkitdbsecuritygroup" --cidr ${webserver_private_ip}/32
+           done
+       fi
 
        /usr/bin/aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --ip-permissions IpProtocol=tcp,FromPort=${SSH_PORT},ToPort=${SSH_PORT},IpRanges="[{CidrIp=${BUILD_CLIENT_IP}/32}]"
 
@@ -511,6 +527,8 @@ then
        then
            /usr/bin/aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --protocol tcp --source-group ${security_group_id} --port 2049 --cidr 0.0.0.0/0
        fi
+       
+
        
        /usr/bin/aws ec2 revoke-security-group-ingress --group-id ${security_group_id} --ip-permissions IpProtocol=tcp,FromPort=0,ToPort=65535,IpRanges=[{CidrIp=0.0.0.0/0}]
        /usr/bin/aws ec2 revoke-security-group-ingress --group-id ${security_group_id} --ip-permissions IpProtocol=tcp,FromPort=${SSH_PORT},ToPort=${SSH_PORT},IpRanges=[{CidrIp=0.0.0.0/0}]
