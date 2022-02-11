cc42d7231c5e3a8c58c1a7236e73c125dc7910bf
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-11 21:45:05 +0000

    Update HardcoreAgileDeploymentToolkit.sh

--- HardcoreAgileDeploymentToolkit.sh
+++ HardcoreAgileDeploymentToolkit.sh
@@ -321,6 +321,8 @@ start=`/bin/date +%s`
 /bin/chmod 400 ~/.ssh/${SERVER_USER}.key
 /bin/mv ~/.ssh/config ~/.ssh/config.${SERVER_USER}
 
+. ${BUILD_HOME}/buildscripts/BuildAndDeployDBaaS.sh
+
 if ( [ "${AUTOSCALE_FROM_SNAPSHOTS}" = "1" ] )
 then
     if ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] )
