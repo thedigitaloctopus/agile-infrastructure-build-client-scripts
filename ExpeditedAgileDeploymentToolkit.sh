6d0ecb3660145b1b46c0cd256884cc186e0600ec
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-11 21:43:52 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -506,7 +506,7 @@ start=`/bin/date +%s`
 /bin/chmod 400 ~/.ssh/${SERVER_USER}.key
 /bin/mv ~/.ssh/config ~/.ssh/config.${SERVER_USER}
 
-. ${BUILD_HOME}/buildscripts/CheckDeployDBaaS.sh
+. ${BUILD_HOME}/buildscripts/BuildAndDeployDBaaS.sh
 
 if ( [ "${AUTOSCALE_FROM_SNAPSHOTS}" = "1" ] )
 then
