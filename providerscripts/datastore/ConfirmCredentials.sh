0ed0059ad937e98529ab00804765ffae40cb1f42
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-01 17:46:59 +0000

    Create ConfirmCredentials.sh

---
+++ providerscripts/datastore/ConfirmCredentials.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+##############################################################################
+# Author: Peter Winter
+# Date  : 13/07/2016
+# Description : This script will copy our generated config file for a particular
+# provider over to our new machine
+###############################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#################################################################################
+#################################################################################
+#set -x
+
+if ( [ "${DATASTORE_CHOICE}" = "amazonS3" ] || [ "${DATASTORE_CHOICE}" = "digitalocean" ] || [ "${DATASTORE_CHOICE}" = "exoscale" ] || [ "${DATASTORE_CHOICE}" = "linode" ] || [ "${DATASTORE_CHOICE}" = "vultr" ] )
+then
+    config_bucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`-config"
+    
+    if ( [ "`/usr/bin/s3cmd ls s3://${config_bucket}`" != "" ] )
+    then
+        credentials_confirmed="0"
+        if ( [ "`/usr/bin/s3cmd ls s3://${config_bucket}/credentials/shit | /usr/bin/awk -F'/' '{print $NF}'`" = "shit" ] )
+        then
+            credentials_confirmed="1"
+        fi
+    fi
+fi
