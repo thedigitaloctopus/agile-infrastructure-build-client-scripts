b42f892b93bc0be300daa119f6b56379f96a4eca
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-13 18:17:50 +0000

    Update BuildAndDeployDBaaS.sh

--- buildscripts/BuildAndDeployDBaaS.sh
+++ buildscripts/BuildAndDeployDBaaS.sh
@@ -211,9 +211,9 @@ then
         machine_type="`/bin/echo ${DATABASE_DBaaS_INSTALLATION_TYPE} | /usr/bin/awk -F':' '{print $5}'`"
         
         status "Your database is being provisioned, please wait....."
-        DATABASE_ID="`/usr/local/bin/linode-cli --json databases mysql-list | jq ".[] | select(.[\"label\"] | contains (\"${label}\")) | .id"`"
+        DATABASE_ID="`/usr/local/bin/linode-cli --json databases mysql-list | jq ".[] | select(.[\\"label\\"] | contains (\\"${label}\\")) | .id"`"
         
-        if ( [ "${database_id}" = "" ] )
+        if ( [ "${DATABASE_ID}" = "" ] )
         then
             /usr/local/bin/linode-cli databases mysql-create --label ${label} --engine ${engine} --cluster_size ${cluster_size} --region ${db_region} --type ${machine_type}
             DATABASE_ID="`/usr/local/bin/linode-cli --json databases mysql-list | jq ".[] | select(.[\\"label\\"] | contains (\\"${label}\\")) | .id"`"
