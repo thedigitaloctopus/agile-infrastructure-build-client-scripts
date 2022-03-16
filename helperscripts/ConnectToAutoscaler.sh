9a583cf4855104d4441ea3a0c151a3472f7bb747
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-16 18:40:40 +0000

    Update ConnectToAutoscaler.sh

--- helperscripts/ConnectToAutoscaler.sh
+++ helperscripts/ConnectToAutoscaler.sh
@@ -61,7 +61,6 @@ fi
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
 
-#AUTOSCALER_IP="`/bin/ls ${BUILD_HOME}/runtimedata/ips/${CLOUDHOST}/${BUILD_IDENTIFIER}/ASIP* | /usr/bin/awk -F':' '{print $NF}'`"
 ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
 
 if ( [ "${ips}" = "" ] )
