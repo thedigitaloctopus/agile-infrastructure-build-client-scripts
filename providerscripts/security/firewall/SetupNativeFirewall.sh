d62c5f4303bb0d4f3b216e7e5bab83f5d577bf65
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-16 19:06:23 +0000

    Update SetupNativeFirewall.sh

--- providerscripts/security/firewall/SetupNativeFirewall.sh
+++ providerscripts/security/firewall/SetupNativeFirewall.sh
@@ -38,16 +38,6 @@ then
 
         firewall_id="`/usr/local/bin/doctl -o json compute firewall list | jq '.[] | select (.name == "adt" ).id' | /bin/sed 's/"//g'`"
 
-     #   server_type="autoscaler"
-     #   autoscaler_ips="`/usr/local/bin/doctl compute droplet list | /bin/grep ${server_type} | /usr/bin/awk -F'    ' '{print $3}' | /bin/sed 's/ //g'`"
-     #   autoscaler_private_ips="`/usr/local/bin/doctl compute droplet list | /bin/grep ${server_type} | /usr/bin/awk -F'    ' '{print $4}' | /bin/sed 's/ //g'`"
-     #   server_type="webserver"
-     #   webserver_ip="`/usr/local/bin/doctl compute droplet list | /bin/grep ${server_type} | /usr/bin/awk -F'    ' '{print $3}' | /bin/sed 's/ //g'`"
-     #   webserver_private_ip="`/usr/local/bin/doctl compute droplet list | /bin/grep ${server_type} | /usr/bin/awk -F'    ' '{print $4}' | /bin/sed 's/ //g'`"
-     #   server_type="database"
-     #   database_ip="`/usr/local/bin/doctl compute droplet list | /bin/grep ${server_type} | /usr/bin/awk -F'    ' '{print $3}' | /bin/sed 's/ //g'`"
-     #   database_private_ip="`/usr/local/bin/doctl compute droplet list | /bin/grep ${server_type} | /usr/bin/awk -F'    ' '{print $4}' | /bin/sed 's/ //g'`"
-     
         autoscaler_ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh autoscaler ${CLOUDHOST}`"
         webserver_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh webserver ${CLOUDHOST}`"
         database_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh database ${CLOUDHOST}`"
@@ -132,7 +122,6 @@ then
         droplet_ids="${autoscaler_ids} ${webserver_id} ${database_id}"
         droplet_ids="`/bin/echo ${droplet_ids} | /bin/sed 's/^ //g' | /bin/sed 's/ $//g' | /bin/sed 's/  / /g' | /bin/sed 's/ /,/g'`"
         
-      #  /usr/local/bin/doctl compute firewall add-droplets ${firewall_id} --droplet-ids ${autoscaler_id},${webserver_id},${database_id}
          /usr/local/bin/doctl compute firewall add-droplets ${firewall_id} --droplet-ids ${droplet_ids}
 
     elif ( [ "${PRE_BUILD}" = "1" ] )
@@ -164,25 +153,6 @@ then
         /usr/bin/exo compute security-group rule add adt --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
     elif ( [ "${PRE_BUILD}" = "0" ] )
     then
-       # server_type="autoscaler"
-       # autoscaler_ip="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_server_type "${server_type}" '(.virtualmachine[] | select(.displayname | contains($tmp_server_type)) | .publicip)' | /bin/sed 's/"//g'`"
-       # vmid="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_ip_address "${autoscaler_ip}" '(.virtualmachine[].nic[] | select(.ipaddress == $tmp_ip_address) | .id)' | /bin/sed 's/"//g'`"
-       # vmid2="`/usr/local/bin/cs listNics | jq --arg tmp_virtual_machine_id "${vmid}" '(.nic[] | select(.id == $tmp_virtual_machine_id) | .virtualmachineid)' | /bin/sed 's/"//g'`"
-       # autoscaler_private_ip="`/usr/local/bin/cs listNics | jq --arg tmp_virtual_machine_id "${vmid2}" '(.nic[] | select(.isdefault == false and .virtualmachineid == $tmp_virtual_machine_id) | .ipaddress)' | /bin/sed 's/"//g'`"
-
-       # server_type="webserver"
-       # webserver_ip="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_server_type "${server_type}" '(.virtualmachine[] | select(.displayname | contains($tmp_server_type)) | .publicip)' | /bin/sed 's/"//g'`"
-       # vmid="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_ip_address "${webserver_ip}" '(.virtualmachine[].nic[] | select(.ipaddress == $tmp_ip_address) | .id)' | /bin/sed 's/"//g'`"
-       # vmid2="`/usr/local/bin/cs listNics | jq --arg tmp_virtual_machine_id "${vmid}" '(.nic[] | select(.id == $tmp_virtual_machine_id) | .virtualmachineid)' | /bin/sed 's/"//g'`"
-       # webserver_private_ip="`/usr/local/bin/cs listNics | jq --arg tmp_virtual_machine_id "${vmid2}" '(.nic[] | select(.isdefault == false and .virtualmachineid == $tmp_virtual_machine_id) | .ipaddress)' | /bin/sed 's/"//g'`"
-
-       # server_type="database"
-       # database_ip="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_server_type "${server_type}" '(.virtualmachine[] | select(.displayname | contains($tmp_server_type)) | .publicip)' | /bin/sed 's/"//g'`"
-       # vmid="`/usr/local/bin/cs listVirtualMachines | /usr/bin/jq --arg tmp_ip_address "${database_ip}" '(.virtualmachine[].nic[] | select(.ipaddress == $tmp_ip_address) | .id)' | /bin/sed 's/"//g'`"
-       # vmid2="`/usr/local/bin/cs listNics | jq --arg tmp_virtual_machine_id "${vmid}" '(.nic[] | select(.id == $tmp_virtual_machine_id) | .virtualmachineid)' | /bin/sed 's/"//g'`"
-       # database_private_ip="`/usr/local/bin/cs listNics | jq --arg tmp_virtual_machine_id "${vmid2}" '(.nic[] | select(.isdefault == false and .virtualmachineid == $tmp_virtual_machine_id) | .ipaddress)' | /bin/sed 's/"//g'`"
-
-
         autoscaler_ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh autoscaler ${CLOUDHOST}`"
         webserver_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh webserver ${CLOUDHOST}`"
         database_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh database ${CLOUDHOST}`"
@@ -191,7 +161,6 @@ then
         webserver_private_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh webserver ${CLOUDHOST}`"
         database_private_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh database ${CLOUDHOST}`"
 
-       # if ( [ "${autoscaler_ip}" != "" ] )
         for ip in ${autoscaler_ips}
         do
             /usr/bin/exo compute security-group rule add adt --network ${ip}/32 --port ${SSH_PORT}
@@ -220,8 +189,6 @@ then
             /usr/bin/exo compute security-group rule add adt --network ${database_private_ip}/32 --port ${DB_PORT}
         fi
 
-       # /usr/bin/exo compute security-group rule add adt --network 0.0.0.0/0 --port 443
-      #  /usr/bin/exo compute security-group rule add adt --network 0.0.0.0/0 --port 80
         /usr/bin/exo compute security-group rule add adt --network ${BUILD_CLIENT_IP}/32 --port 22
         /usr/bin/exo compute security-group rule add adt --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
         
@@ -256,18 +223,7 @@ then
    
         /usr/local/bin/linode-cli firewalls create --label "adt" --rules.inbound_policy DROP   --rules.outbound_policy ACCEPT
         firewall_id="`/usr/local/bin/linode-cli --json firewalls list | jq '.[] | select (.label == "adt" ).id'`"
-
-
-      #  server_type="autoscaler"
-      #  autoscaler_ips="`/usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep -v "192.168"`"
-      #  autoscaler_private_ips="`/usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep "192.168"`"
-      #  server_type="webserver"
-      #  webserver_ip="`/usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep -v "192.168"`"
-      #  webserver_private_ip="`/usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep "192.168"`"
-      #  server_type="database"
-      #  database_ip="`/usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep -v "192.168"`"
-      #  database_private_ip="`/usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep "192.168"`"
-    
+   
         autoscaler_ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh autoscaler ${CLOUDHOST}`"
         webserver_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh webserver ${CLOUDHOST}`"
         database_ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh database ${CLOUDHOST}`"
@@ -329,9 +285,7 @@ then
         else 
              /usr/local/bin/linode-cli firewalls rules-update --inbound  "[{\"addresses\":{\"ipv4\":[${ips}]},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT},${DB_PORT},22\"},{\"addresses\":{\"ipv4\":[${alldnsproxyips}]},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"443,80\"},{\"addresses\":{\"ipv4\":[\"0.0.0.0/0\"]},\"action\":\"ACCEPT\",\"protocol\":\"ICMP\"}]" ${firewall_id}
         fi
-        
-        #/usr/local/bin/linode-cli firewalls rules-update --inbound  "[{\"addresses\":{\"ipv4\":[\"0.0.0.0/0\"]},\"action\":\"ACCEPT\",\"protocol\":\"TCP\",\"ports\":\"${SSH_PORT},${DB_PORT},443,80,22\"},{\"addresses\":{\"ipv4\":[\"0.0.0.0/0\"]},\"action\":\"ACCEPT\",\"protocol\":\"ICMP\"}]" ${firewall_id}
-    
+            
         autoscaler_id="`/usr/local/bin/linode-cli --json linodes list | jq '.[] | select (.label | contains ("autoscaler")).id'`"
         webserver_id="`/usr/local/bin/linode-cli --json linodes list | jq '.[] | select (.label | contains ("webserver")).id'`"
         database_id="`/usr/local/bin/linode-cli --json linodes list | jq '.[] | select (.label | contains ("database")).id'`"
@@ -357,8 +311,8 @@ then
 
     if ( [ "${PRE_BUILD}" = "0" ] )
     then
-        firewall_id="`/usr/bin/vultr firewall group list | /usr/bin/tail -n +2 | /bin/grep -w 'adt$' | /usr/bin/awk '{print $1}'`"
-      #  private_network_ip="`/usr/bin/vultr network list | grep "Default private network" | /usr/bin/awk '{print $6}'`/24"
+    
+       firewall_id="`/usr/bin/vultr firewall group list | /usr/bin/tail -n +2 | /bin/grep -w 'adt$' | /usr/bin/awk '{print $1}'`"
        
        if ( [ "${firewall_id}" != "" ] )
         then
@@ -389,7 +343,6 @@ then
        do              
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${machine_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${DB_PORT} --protocol tcp --size 32 --type v4 -s ${machine_ip}
-         #  /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${BUILD_CLIENT_IP}
        done
        
@@ -407,7 +360,6 @@ then
        do              
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${DB_PORT} --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
-      #     /usr/bin/vultr firewall rule create --id ${firewall_id} --port 22 --protocol tcp --size 32 --type v4 -s ${machine_private_ip}
            /usr/bin/vultr firewall rule create --id ${firewall_id} --port ${SSH_PORT} --protocol tcp --size 32 --type v4 -s ${BUILD_CLIENT_IP}
        done
        
@@ -488,12 +440,11 @@ then
        do
             /usr/bin/aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --protocol tcp  --port 22 --cidr ${autoscaler_ip}/32
        done
-      
-              
-        autoscaler_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh autoscaler ${CLOUDHOST}`"
-        webserver_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh webserver ${CLOUDHOST}`"
-        database_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh database ${CLOUDHOST}`"
-        machine_private_ips="${autoscaler_private_ips} ${webserver_private_ips} ${database_private_ips}"
+       
+       autoscaler_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh autoscaler ${CLOUDHOST}`"
+       webserver_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh webserver ${CLOUDHOST}`"
+       database_private_ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh database ${CLOUDHOST}`"
+       machine_private_ips="${autoscaler_private_ips} ${webserver_private_ips} ${database_private_ips}"
        
        for machine_private_ip in ${machine_private_ips}
        do              
